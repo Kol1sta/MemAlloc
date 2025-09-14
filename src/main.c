6824e66e497a0c0d65b629e0a8a5fb453f56311a
Kol1sta <wiltshirett@gmail.com>
2025-09-15 01:46:39 +0300

    Added windows memory free

--- src/main.c
+++ src/main.c
@@ -6,23 +6,26 @@
 
 typedef struct block_t block_t;
 
-struct block_t {
+struct block_t
+{
     size_t size;
     uint8_t is_free;
-    block_t* next;
+    block_t *next;
 };
 
-static block_t* free_list = NULL;
+static block_t *free_list = NULL;
 
-void* init_allocator(size_t size) {
-    void* memory = VirtualAlloc(NULL, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+void *init_allocator(size_t size)
+{
+    void *memory = VirtualAlloc(NULL, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 
-    if(memory == NULL) {
-        perror("Failed to allocate memory 1");
+    if (memory == NULL)
+    {
+        fprintf(stderr, "Failed to allocate memory");
         return NULL;
     }
 
-    block_t* block = (block_t*)memory;
+    block_t *block = (block_t *)memory;
     block->size = size - sizeof(block_t);
     block->is_free = 1;
     block->next = NULL;
@@ -31,21 +34,26 @@ void* init_allocator(size_t size) {
     return memory;
 }
 
-void* mem_alloc(size_t size) {
-    if(free_list == NULL && init_allocator((size > 1024 * 1024) ? size * 2 : 1024 * 1024) == NULL) {
-        perror("Failed to allocate memory 2");
+void *mem_alloc(size_t size)
+{
+    if (free_list == NULL && init_allocator((size > 1024 * 1024) ? size * 2 : 1024 * 1024) == NULL)
+    {
+        fprintf(stderr, "Failed to allocate memory");
         return NULL;
     }
 
-    block_t* current = free_list;
-    block_t* previous = NULL;
+    block_t *current = free_list;
+    block_t *previous = NULL;
 
-    while(current != NULL) {
-        if(current->is_free == 1 && current->size >= size) {
+    while (current != NULL)
+    {
+        if (current->is_free == 1 && current->size >= size)
+        {
             current->is_free = 0;
 
-            if(current->size > size + sizeof(block_t) + 16) {
-                block_t* next_block = (block_t*)((char*)(current + 1) + size);
+            if (current->size > size + sizeof(block_t) + 16)
+            {
+                block_t *next_block = (block_t *)((char *)(current + 1) + size);
                 next_block->is_free = 1;
                 next_block->size = current->size - size - sizeof(block_t);
 
@@ -53,26 +61,52 @@ void* mem_alloc(size_t size) {
                 current->next = next_block;
             }
 
-            return (void*)(current + 1);
+            return (void *)(current + 1);
         }
 
         previous = current;
         current = current->next;
     }
 
-    perror("Failed to allocate memory 3");
+    fprintf(stderr, "No suitable block found for size");
     return NULL;
 }
 
-int main(int argc, char* argv[]) {
-    int* a = mem_alloc(sizeof(int));
+void mem_free(void *ptr)
+{
+    if (ptr == NULL)
+    {
+        fprintf(stderr, "Attempt to free NULL pointer\n");
+        return;
+    }
+
+    block_t *block = (block_t *)ptr - 1;
+    block->is_free = 1;
+
+    block_t* current = free_list;
+    while(current != NULL && current->next != NULL) {
+        if(current->is_free && current->next->is_free) {
+            current->size += current->next->size + sizeof(block_t);
+            current->next = current->next->next;
+        } else {
+            current = current->next;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int *a = mem_alloc(sizeof(int));
     *a = 2;
 
-    char* b = (char*)mem_alloc(sizeof(char) * 20);
+    char *b = (char *)mem_alloc(sizeof(char) * 20);
     strcpy(b, "Hello, world!");
 
     printf("%p, %d\n", a, *a);
     printf("%p, %s\n", b, b);
+
+    mem_free(a);
+    mem_free(b);
 
     return 0;
 }
